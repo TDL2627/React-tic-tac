5188785f8a7da655a5bb847a0a179ee8fee22643
TDL2627 <the.dark.lord.2627@gmail.com>
2022-04-26 15:42:16 +0200

    update 3

--- src/index.js
+++ src/index.js
@@ -23,10 +23,13 @@ function Square(props) {
 
 handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i]= this.state.xIsNext ? 'X' :'O';
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
-    squares:squares,
-    xIsNext: !this.state.xIsNext,
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
     });
 }
 
